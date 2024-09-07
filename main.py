b6ed10451be49bfa0bcb15f60ed338c307b1d8c3
MrChuw <jgabrielmonteiro@hotmail.com>
2024-09-07 05:53:51 -0300

    Ops

--- main.py
+++ main.py
@@ -130,5 +130,5 @@ async def get_hsl_color(request: Request, cmyk_code: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8383, log_level="info", reload=True)
-    # uvicorn.run("main:app", host="0.0.0.0", port=8383, log_level="info", workers=4)
+    # uvicorn.run("main:app", host="0.0.0.0", port=8383, log_level="info", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8383, log_level="info", workers=4)
